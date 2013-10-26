921cdb16c3f404214dbfe1a7081dce74442a71bb
Tim Cooper <tjkoopa@gmail.com>
2013-10-26 21:56:36 +0100

    Added concat to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,23 +15,10 @@ module.exports = function(grunt) {
 						dest: 'src/min',   // Destination path prefix.
 					},
 				]
-			},
-			libs: {
-				files: [
-					{
-						expand: true,     // Enable dynamic expansion.
-						cwd: 'phaser',
-						src: ['*.js'], // Actual pattern(s) to match.
-						dest: 'phaser/min',   // Destination path prefix.
-					},
-				]
-			},
-			modules: {
-				files: {}
 			}
 		},
 		jshint: {
-			files: ['src'],  //Just jshint on the files we create not libs
+			files: ['src/*.js'],  //Just jshint on the files we create not libs
 			options: {
 				// options here to override JSHint defaults
 				globals: {
@@ -39,6 +26,15 @@ module.exports = function(grunt) {
 					module: true,
 					document: true
 				}
+			}
+		},
+		concat: {
+			options: {
+				separator: ';'
+			},
+			dist: {
+				src: ['lib/phaser.min.js', 'src/min/*.js'],
+				dest: 'game.js'
 			}
 		},
 		imagemin: {
@@ -61,8 +57,9 @@ module.exports = function(grunt) {
 
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-	grunt.registerTask('default', ['jshint', 'uglify', 'imagemin']);
+	grunt.registerTask('default', ['jshint', 'uglify', 'concat', 'imagemin']);
 
 };
