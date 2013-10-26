8dc967a52386b59eca57facf8bbe3528cf64ee09
Tim Cooper <tjkoopa@gmail.com>
2013-10-26 16:44:38 +0100

    Vertical movement
    Switch between 3 vertical levels

--- src/Game.js
+++ src/Game.js
@@ -31,6 +31,21 @@ Main.Game.prototype = {
 
     this.player.body.acceleration.x = 50;
 
+    this.setVerticalLevel();
+
+  },
+
+  setVerticalLevel: function () {
+
+    var mouseLevel = this.game.input.activePointer.y;
+    if(mouseLevel < 100) {
+      this.player.centerOn(this.player.center.x, 41);
+    }else if(mouseLevel < 200) {
+      this.player.centerOn(this.player.center.x, 141);
+    }else if(mouseLevel < 300) {
+      this.player.centerOn(this.player.center.x, 241);
+    }
+
   }
 
 }
